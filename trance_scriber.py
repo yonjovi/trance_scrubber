82716ec24e6bc68ea54c11632a654f01361cfeab
yono <yonpython@gmail.com>
2023-08-31 21:58:33 +1000

    updating pytube dependency

--- trance_scriber.py
+++ trance_scriber.py
@@ -15,6 +15,10 @@ formatted_result_app = None
 p_url = None
 yt_video = None
 
+# with st.sidebar:
+#     st.subheader("Open AI API Key (Optional)")
+#     OPEN_AI_API_KEY = st.text_input("Enter your Open AI API Key here:", type="password")
+
 
 def upload_audio_to_s3(file, bucket, s3_file):
     try:
@@ -26,23 +30,23 @@ def upload_audio_to_s3(file, bucket, s3_file):
         return False
 
 
-def summarise(text_input):
-    openai.api_key = st.secrets['openai_api_key']
+def summarise(text_input, yt_title):
+    try:
+        openai.api_key = OPEN_AI_API_KEY
+        yt_title = yt_title
+        response = openai.Completion.create(
+            model='text-davinci-002',
+            prompt=f'Summarize this transcribed text from the YouTube video titled "{yt_title}" in dot points:\n\n{text_input}',
+            temperature=0.7,
+            max_tokens=4096,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
 
-    # text_input = text_input
-    prompt = f"Summarize this in dot points:\n\n{text_input}",
-
-    response = openai.Completion.create(
-        model='text-davinci-002',
-        prompt=f'Summarize this in dot points:\n\n{text_input}',
-        temperature=0.7,
-        max_tokens=256,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0
-    )
-
-    return response['choices'][0]['text']
+        return response['choices'][0]['text']
+    except:
+        return st.warning("Please enter Open AI API Key in the sidebar", icon="↖️")
 
 
 st.header('Transcribe this!')
@@ -129,14 +133,31 @@ with st.form('silent-whisper', clear_on_submit=False):
 if formatted_result_app is not None and yt_video is not None:
     ste.download_button('Download', formatted_result_app,
                         f'{yt_video.title} transcribed.txt')
-    st.subheader(f'Summary of text transcribed from "{yt_video.title}":')
-    with st.spinner('Summarising...🌞🌞🌞'):
-        summary = summarise(result_app)
-        st.write(summary)
+    # sum_or_not = st.radio(
+    #     "Would you like to summarise the text using Open AI? (Open AI API Key required)",
+    #     ("Yes", "No")
+    # )
+    # if sum_or_not == "Yes":
+    #     try:
+    #         st.subheader(f'Summary of text transcribed from "{yt_video.title}":')
+    #         with st.spinner('Summarising...🌞🌞🌞'):
+    #             summary = summarise(text_input=result_app, yt_title=yt_video.title)
+    #             st.write(summary)
+    #     except:
+    #         st.info("Open AI API fees are not covered by this app unfortunately", icon="ℹ️")
 elif formatted_result_app is not None and uploaded_file is not None:
     ste.download_button('Download', formatted_result_app,
                         f'{uploaded_file.name} transcribed.txt')
-    st.subheader(f'Summary of text transcribed from "{uploaded_file.name}":')
-    with st.spinner('Summarising...🌞🌞🌞'):
-        summary = summarise(result_app)
-        st.write(summary)
+    # sum_or_not = st.radio(
+    #     "Would you like to summarise the text using Open AI? (Open AI API Key required)",
+    #     ("Yes", "No")
+    # )
+    # if sum_or_not == "Yes":
+    #     try:
+    #         st.subheader(f'Summary of text transcribed from "{uploaded_file.name}":')
+    #         with st.spinner('Summarising...🌞🌞🌞'):
+    #             summary = summarise(text_input=result_app, yt_title=yt_video.title)
+    #             st.write(summary)
+    #     except:
+    #         st.info("Open AI API fees are not covered by this app unfortunately", icon="ℹ️")
+    #
